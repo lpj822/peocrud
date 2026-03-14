939384c3c6f5c67912c592f39b18af9f42060f33
LPJay <lpj822@gmail.com>
2026-03-14 14:15:49 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+$books = [
+    [
+        'title' => 'Charitha Thunak',
+        'year' => '1970',
+        'author' => 'K Jayathilaka',
+        'purchaseURL' => 'http://example.com'
+    ],
+    [
+        'title' => 'Hath  Pana',
+        'year' => '1965',
+        'author' => 'Martin Wikrasinghe',
+        'purchaseURL' => 'http://example.com'
+    ],
+    [
+        'title' => 'Hin Seraya',
+        'year' => '1960',
+        'author' => 'Munidasa Kumarathunga',
+        'purchaseURL' => 'http://example.com'
+    ]
+];
+
+
+require 'index.view.php';
