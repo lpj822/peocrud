a4ef966f295c1dff246d15319c9f2fdaa0e10265
LPJay <lpj822@gmail.com>
2026-04-03 13:01:45 +0100

    var dump variable

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
 
 $heading = "Home";
+
+function dd($value) {
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+    die();
+}
+
+dd($_SERVER['REQUEST_URI']);
+
 require ('views/index.view.php');
