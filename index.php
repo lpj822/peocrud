42786060b8b0f37d18aaa8fd394633f2c6e3a3d3
LPJay <lpj822@gmail.com>
2026-04-05 13:19:41 +0100

    Checkpoint : connect to mysql database

--- index.php
+++ index.php
@@ -8,4 +8,25 @@
 
 require('functions.php');
 
-require ('router.php');
+//require ('router.php');
+
+
+// Testing :  Connect to MySql database
+
+$dsn = "mysql:host=localhost;dbname=peo_blog;charset=utf8mb4";
+
+$pdo = new PDO($dsn, "root", "root");
+
+$query = $pdo->prepare("select * from `post`");
+
+$query->execute();
+
+$posts = $query->fetchAll(PDO::FETCH_ASSOC);
+dd($posts);
+
+
+
+
+
+
+
